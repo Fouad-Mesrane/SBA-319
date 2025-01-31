fd2ded3eb115fe3aac2c40a973055280a392bfb9
fouad mesrane <fouadmesrane@gmail.com>
2025-01-31 10:20:09 -0700

    created specefic routes for each request

---
+++ routes/usersRoute.js
@@ -0,0 +1,21 @@
+import e from "express";
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from "../controllers/userController";
+
+const router = e.Router()
+
+
+
+// get all users route 
+router.get("/", getAllUsers)
+
+// create user route
+router.post("/", createUser)
+// get single user
+router.get("/:id", getUserById)
+// update a user
+router.put("/:id", updateUser)
+//delete a user
+router.delete("/:id", deleteUser)
+
+
+export default router
